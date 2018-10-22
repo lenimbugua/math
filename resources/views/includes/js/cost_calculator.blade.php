6ed3e31ec4b07d12d391c1fd1ec6f6e65fa3dc51
lenimbugua <lenimbugua@gmail.com>
2018-10-22 12:31:38 +0300

    Number of pages, academic level and deadline complete

--- resources/views/includes/js/cost_calculator.blade.php
+++ resources/views/includes/js/cost_calculator.blade.php
@@ -26,47 +26,90 @@
     }
     function deadline(){
         let deadline = document.getElementById('deadline');
-        console.log(pages);
+        let urgency = deadline.options[deadline.selectedIndex].value;
+
+        switch (urgency) {
+          case '6 Hours':
+                let value=parseInt(document.getElementById('numberofpages').value);
+                if (value>4){
+                    value=5;
+                    document.getElementById('numberofpages').value=value;
+                    document.getElementById('numberofpages').disabled=true;
+                    document.getElementById('add').disabled=true;
+                    return 32;
+                }
+                else{
+                    document.getElementById('numberofpages').disabled=false;
+                    document.getElementById('add').disabled=false;
+                    return 32;
+                }
+                
+            
+            break;
+          case '12 Hours':
+            let value1=parseInt(document.getElementById('numberofpages').value);
+            if (value1>9){
+                value1=10;
+                document.getElementById('numberofpages').value=value1;
+                return 28;
+            }
+            return 28;
+            break;
+          case '24 Hours':
+            let value2=parseInt(document.getElementById('numberofpages').value);
+            if (value2>17){
+                value2=18;
+                document.getElementById('numberofpages').value=value2;
+                return 24;
+            }
+            return 24;            
+            break;
+          case '2 Days':
+            return 20;
+            break;
+          case '3 Days':
+            return 16;
+            break;
+          case '5 Days':
+            return 12;
+            break;
+          case '7 Days':
+            return 8;            
+            break;
+          case '9 Days':
+            return 4;
+            break;
+          case '14 Days':
+            return 0;
+            break;
+          default:
+            console.log('Sorry, we are out of stock');
+        }
     }
     function pages(){
         let pages = document.getElementById('numberofpages').value;
         console.log(pages);
     }
     function calculateCost(){
+        document.getElementById('add').disabled=false;
         let cost = 8;
         
         
-        cost += academicLevel();
-        return cost
-        // y = 88x +19;
-        // let y = 0;
-        // let e = document.getElementById('academiclevel');   
-        // let x = e.options[e.selectedIndex].value;
-
-        
-        // if (x==='0'){
-        //     document.getElementById('totalcost').innerHTML = "$ 00.00";
-        // }
-        // else{
-           
-            // y = (88*x) + 19;
-            // console.log( '$ '+y);
-            
-            //  
-        // }
-        
+        cost += academicLevel() + deadline();
+        return cost;        
     }
     function onInputCalculateCost(){
+        d=calculateCost();
         let value=parseInt(document.getElementById('numberofpages').value);
-        value *= calculateCost();
-        document.getElementById('totalcost').innerHTML = "$ "+value+".00";
+        value *= d;
+        return value;
     }
 
     function addQuestion()
     {
         let value=parseInt(document.getElementById('numberofpages').value);
         value = value+1;
-        let r =document.getElementById('numberofpages').value=value;
+        document.getElementById('numberofpages').value=value;
 
 
         value *= calculateCost();
@@ -84,8 +127,12 @@
         }  
 
          value2 *= calculateCost();
-        document.getElementById('totalcost').innerHTML = "$ "+value2+".00";      
-        
+        document.getElementById('totalcost').innerHTML = "$ "+value2+".00";       
+    }
+
+    function displayCost(){
+        let cost = onInputCalculateCost();
+        document.getElementById('totalcost').innerHTML = "$ "+cost+".00";
     }
         
 </script>
---
+++ resources/views/includes/paper_type.blade.php
@@ -0,0 +1,48 @@
+<div class="calculator-options">
+  <div class="academic-level">
+    <form>
+    <label for="academiclevel" class="mb-0"><small><strong>Paper Type</strong></small></label>                                      
+      <select class="form-control" id="paperType" onchange="displayCost()">
+        <option value="Admission Essay">Admission Essay</option>
+        <option value="12 Hours">Annotated Bibliography</option>
+        <option value="24 Hours">Application Letter</option>
+        <option value="2 Days">Argumentative Essay</option>
+        <option value="3 Days">Article</option>
+        <option value="5 Days">Article review</option>
+        <option value="7 Days">Biography</option>
+        <option value="9 Days">Business Plan</option>
+        <option value="14 Days">Case Study</option>
+        <option value="Admission Essay">Course Work</option>
+        <option value="12 Hours">Cover Letter</option>
+        <option value="24 Hours">Creative Writing</option>
+        <option value="2 Days">Critical Thinking</option>
+        <option value="3 Days">Curriculum Vitae</option>
+        <option value="5 Days">Dissertation</option>
+        <option value="7 Days">Dissertation Proposal</option>
+        <option value="7 Days">Dissertation Abstract</option>
+        <option value="9 Days">Dissertation Introduction</option>
+        <option value="14 Days">Dissertation Hypothesis</option>
+        <option value="7 Days">Dissertation Chapter</option>
+        <option value="9 Days">Dissertation Methodology</option>
+        <option value="9 Days">Dissertation Results</option>
+        <option value="9 Days">Dissertation Conclusion</option>
+        <option value="14 Days">Essay</option>
+        <option value="14 Days">Literature Review</option>
+        <option value="14 Days">Movie Review</option>
+        <option value="14 Days">Personal Statement</option>
+        <option value="14 Days">Presentation</option>
+        <option value="14 Days">Problem Solving</option>
+        <option value="14 Days">Recommendation Letter</option>
+        <option value="14 Days">Report</option>
+        <option value="14 Days">Research Proposal</option>
+        <option value="14 Days">Research Paper</option>
+        <option value="14 Days">Resume</option>
+        <option value="14 Days">Speech</option>
+        <option value="14 Days">Term Paper</option>
+        <option value="14 Days">Thesis</option>
+        <option value="14 Days">Thesis Proposal</option>
+        <option value="14 Days">Thesis Statement</option>
+      </select>              
+    </form>
+  </div>
+</div>
