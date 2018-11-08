f1d12d049a0c7f758499fb48c587610491d47640
lenimbugua <lenimbugua@gmail.com>
2018-11-08 22:51:11 +0300

    Revision and approval button

--- resources/views/pages/clients/orders/show/files.blade.php
+++ resources/views/pages/clients/orders/show/files.blade.php
@@ -44,41 +44,60 @@
      </div>  
       <div class="row no-gutter d-flex justify-content-center">
           
-            <div class="card p-4 mb-5  messages rounded" style="width: 90%">
-            <h6 class="card-title text-success">Solutions</h6> 
-             <table class="table table-sm table-bordered">            
-               @php
-                 $count2=0;
+            <div class="card p-4 mb-5  messages rounded " style="width: 90%">
+              <h6 class="card-title text-success">Solutions</h6> 
+              <div class="row">
+                <div class="col d-flex justify-content-end">
+                                 <table class="table table-sm table-bordered" style="width: 18rem">  
+               <thead>
+                      <tr>
+                        <th scope="col"></th>
+                        <th scope="col" colspan="3">Files</th>                  
+                      </tr>
+                    </thead>
+                    <tbody>          
+                 @php
+                   $count2=0;
 
-               @endphp
-               
-                  @foreach($files as $file)
+                 @endphp
                     
-                    @if($file->question_or_answer == 'answer')
+                    @foreach($files as $file)
+                      
+                      @if($file->question_or_answer == 'answer')
 
-                    <?php
-                    $count2++;
+                      <?php
+                      $count2++;
 
-                    ?>
-                    <thead>
-                    <tr>
-                      <th scope="col"></th>
-                      <th scope="col" colspan="3">Action</th>                  
-                    </tr>
-                  </thead>
-                  <tbody> 
-                   <tr>
-                      <th scope="row">{{$count2++}}</th>
-                      <td><a href="{{route('downloadfiles',$file->id)}}" type="button" class="btn btn-primary"> Download <i class="fas fa-download"></i></a></td>
-                      <td><a href="{{route('revisions.create',$id)}}" type="button" class="btn btn-info"> Send Revision Request <i class="fas fa-download"></i></a></td>
-                      <td><a href="{{route('downloadfiles',$file->id)}}" type="button" class="btn btn-success"> Approve<i class="fas fa-download"></i></a></td>
-                    </tr>
-                    @else
-                    <p class="text-danger">Solutions are yet to be uploaded</p>
-                   @endif
-                @endforeach
-                </tbody>
-            </table> 
+                      ?>
+                       
+                     <tr>
+                        <th scope="row">{{$count2}}</th>
+                        <td><a href="{{route('downloadfiles',$file->id)}}" type="button" class="buttons btn btn-primary"> Download <i class="fas fa-download"></i></a></td>
+                        
+                      </tr>
+                      @else
+                      <p class="text-danger">Solutions are yet to be uploaded</p>
+                     @endif
+                  @endforeach
+                  
+                  </tbody>
+              </table>
+                </div>
+                <div class="col p-4">
+                  <div style="width: 15rem">
+                    <div class="row m-3">
+                    <a href="{{route('revisions.create',$id)}}" type="button" class=" buttons btn btn-block btn-info" style="background: #8E2D83; height: 3rem"> Send Revision Request </a>
+                  </div>
+                  <div class="row m-3">
+                    <a href="{{route('downloadfiles',$file->id)}}" type="button" class="buttons btn btn-large btn-block btn-success" style="background:#28A745; height: 3rem"> Approve <i class="fas fa-check"></i></a>
+                  </div>
+                  </div>
+                  
+                  
+                </div>
+              </div>
+
+              <strong>NB</strong> <p>The above files are not editable nor printable until you approve by clicking approve button above according to Pronto Labs <a href="{{ route('legal.blogs',['category'=>'revisionpolicy']) }}" target="blank">Revision Policy</a></p> 
           </div>
         </div>
         <div class="row no-gutter d-flex justify-content-center">
